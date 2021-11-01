a327136bdf43eda3fdcb8955bad3ea557a511c10
Kishin125 <72475864+Kishin125@users.noreply.github.com>
2021-11-01 15:55:55 -0500

    update db

--- Restaurant_app/create_db.sql
+++ Restaurant_app/create_db.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS userCredentials CASCADE;
 DROP TABLE IF EXISTS userInfo CASCADE;
-DROP TABLE IF EXISTS guest CASCADE;
+DROP TABLE IF EXISTS reservation CASCADE;
+DROP TABLE IF EXISTS tableInfo CASCADE;
 CREATE EXTENSION pgcrypto;
 
 CREATE TABLE userCredentials(
@@ -17,13 +18,124 @@ CREATE TABLE userInfo(
 	billAddress TEXT,
 	point integer NOT NULL,
 	preferPayment TEXT NOT NULL,
-	CONTRAINT c_userID_fk FOREIGN KEY (userID) REFERENCES userCredentials(userID) ON DELETE CASCADE)
+	CONSTRAINT c_userID_fk FOREIGN KEY (userID) REFERENCES userCredentials(userID) ON DELETE CASCADE
 );
 
-CREATE TABLE guest(
+CREATE TABLE reservation(
+	reservationID varchar NOT NULL UNIQUE,
 	guestName TEXT NOT NULL,
 	phone integer NOT NULL,
 	email varchar(30) NOT NULL,
 	date DATE NOT NULL,
-	guestNumber integer NOT NULL
+	guestNumber integer NOT NULL,
+	isTraffic TEXT NOT NULL,
+	tablePicked TEXT NOT NULL,
+	reservedTime DATE NOT NULL,
+	userID SERIAL,
+	PRIMARY KEY(reservationID),
+	CONSTRAINT r_userID_fk FOREIGN KEY (userID) REFERENCES userCredentials(userID) ON DELETE CASCADE
+);
+
+CREATE TABLE tableInfo(
+	reservationID varchar,
+	tableCode varchar NOT NULL,
+	seatNum integer NOT NULL,
+	reserved TEXT NOT NULL,
+	CONSTRAINT r_userID_fk FOREIGN KEY (userID) REFERENCES userCredentials(userID) ON DELETE CASCADE
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'A1',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'A2',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'A3',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'A4',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'B1',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'B2',
+    4,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'B3',
+    4,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'B4',
+    4,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'C1',
+    6,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'C2',
+    6,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'C3',
+    6,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'D1',
+    8,
+    'no'
 );
