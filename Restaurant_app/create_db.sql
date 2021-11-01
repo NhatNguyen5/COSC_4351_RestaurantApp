fcacc0e54038cc22d5b792f29b45987941256cfd
Nathan N <nhatnguyen2807@gmail.com>
2021-11-01 15:59:01 -0500

    Merge branch 'main' of https://github.com/NhatNguyen5/COSC_4351_RestaurantApp

--- Restaurant_app/create_db.sql
+++ Restaurant_app/create_db.sql
@@ -1,12 +1,8 @@
 DROP TABLE IF EXISTS userCredentials CASCADE;
 DROP TABLE IF EXISTS userInfo CASCADE;
-DROP TABLE IF EXISTS guest CASCADE;
+DROP TABLE IF EXISTS reservation CASCADE;
 DROP TABLE IF EXISTS tableInfo CASCADE;
-DROP TABLE IF EXISTS tableStatus CASCADE;
-
-/*
 CREATE EXTENSION pgcrypto;
-*/
 
 CREATE TABLE userCredentials(
 	userID SERIAL NOT NULL UNIQUE,
@@ -20,40 +16,126 @@ CREATE TABLE userInfo(
 	fullname TEXT NOT NULL,
 	mailAddress TEXT NOT NULL,
 	billAddress TEXT,
-	points integer NOT NULL,
+	point integer NOT NULL,
 	preferPayment TEXT NOT NULL,
-	CONSTRAINT c_userID_fk FOREIGN KEY (userID) REFERENCES UserCredentials(userID) ON DELETE CASCADE
+	CONSTRAINT c_userID_fk FOREIGN KEY (userID) REFERENCES userCredentials(userID) ON DELETE CASCADE
 );
 
-CREATE TABLE guest(
+CREATE TABLE reservation(
+	reservationID varchar NOT NULL UNIQUE,
 	guestName TEXT NOT NULL,
 	phone integer NOT NULL,
 	email varchar(30) NOT NULL,
 	date DATE NOT NULL,
-	guestNumber integer NOT NULL
+	guestNumber integer NOT NULL,
+	isTraffic TEXT NOT NULL,
+	tablePicked TEXT NOT NULL,
+	reservedTime DATE NOT NULL,
+	userID SERIAL,
+	PRIMARY KEY(reservationID),
+	CONSTRAINT r_userID_fk FOREIGN KEY (userID) REFERENCES userCredentials(userID) ON DELETE CASCADE
 );
-
-CREATE TABLE reservations
 
 CREATE TABLE tableInfo(
-	tableCode TEXT,
-	noOfSeats INTEGER,
-	reserved INTEGER,
-	CONSTRAINT c_userID_fk FOREIGN KEY (userID) REFERENCES UserCredentials(userID) ON DELETE CASCADE
+	reservationID varchar,
+	tableCode varchar NOT NULL,
+	seatNum integer NOT NULL,
+	reserved TEXT NOT NULL,
+	CONSTRAINT r_userID_fk FOREIGN KEY (userID) REFERENCES userCredentials(userID) ON DELETE CASCADE
 );
 
-INSERT INTO tableStatus (tableCode, noOfSeats, reserved) 
-VALUES 
-	 ('A1', 2, 0)
-	,('A2', 2, 0)
-	,('A3', 2, 0)
-	,('A4', 2, 0)
-	,('B1', 4, 0)
-	,('B2', 4, 0)
-	,('B3', 4, 0)
-	,('B4', 4, 0)
-	,('C1', 6, 0)
-	,('C2', 6, 0)
-	,('C3', 6, 0)
-	,('D1', 8, 0);
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'A1',
+    2,
+    'no'
+);
 
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'A2',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'A3',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'A4',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'B1',
+    2,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'B2',
+    4,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'B3',
+    4,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'B4',
+    4,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'C1',
+    6,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'C2',
+    6,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'C3',
+    6,
+    'no'
+);
+
+INSERT INTO tableInfo
+VALUES(
+    NULL,
+    'D1',
+    8,
+    'no'
+);
