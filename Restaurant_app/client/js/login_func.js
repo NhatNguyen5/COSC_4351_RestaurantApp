b6ef4e3a162abc18045dede0f953e288564e2ebe
jhta2000 <ta.jonathan2000@gmail.com>
2021-11-15 11:27:49 -0600

    Register
    Adding register scripts/functions to input user input into database

--- Restaurant_app/client/js/login_func.js
+++ Restaurant_app/client/js/login_func.js
@@ -58,12 +58,26 @@ async function insertUserCred() {
     }
     var user = document.querySelector('#r_user').value;
     var pass = document.querySelector('#r_pass').value;
+    var firstN = document.querySelector('#nameF').value;
+    var lastN = document.querySelector('#nameL').value;
+    var mailA = document.querySelector('#mailA').value;
+    var billA = document.querySelector('#billA').value;
+    var mailE = document.querySelector('mailE').value;
+    var numP = document.querySelector('numP').value;
     var userid = uid;
     try{
         const body = {
             userid: userid,
             user: user, 
-            pass: pass 
+            pass: pass,
+            fullname: firstN + ' ' + lastN,
+            phone: numP,
+            email: mailE,
+            mailaddress: mailA,
+            billaddress: billA,
+            point: 0,
+            preferpayment: 'Valid CreditCard'
+            
         };
         const response = await fetch(`http://localhost:5000/register`, {
             method: "POST",
@@ -71,7 +85,7 @@ async function insertUserCred() {
             body: JSON.stringify(body)
         });
         alert("Registration Successful!");
-        window.location.href = 'login.html';
+        window.location.href = 'register_page.html';
     }catch (err) {
         console.log(err.message);
     }
